5d10ca642ac27653f883527803c410a4af4f11a6
CHOC4HOL1C <zannat795@gmail.com>
2023-05-07 04:06:54 +0600

    to request colors

--- toRequest.php
+++ toRequest.php
@@ -28,11 +28,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Dosis:wght@500&family=Montserrat:wght@500;700&family=Poppins:wght@500;700&display=swap"
+      rel="stylesheet"
+    />
     <link rel="stylesheet" href="toRequest.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <title>Request Subject</title>
 </head>
-<body style="background-color: #2a4d6d;">
+<body style="background-color: #12212f;">
 <div class="formContainer">
         <form method="post">
             <div class="mt-3 mb-2">
